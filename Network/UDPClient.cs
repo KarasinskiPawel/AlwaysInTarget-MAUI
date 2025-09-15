c45eadee617bfc73dc7d0cb6d2aadd3e9193a597
Karasiński Paweł <pawelkarasinski1981@o2.pl>
2025-09-15 15:21:33 +0200

    Version-6.10

--- Network/UDPClient.cs
+++ Network/UDPClient.cs
@@ -197,10 +197,10 @@ namespace AlwaysInTarget.Network
 
             if (!testPrediction)
             {
-                //planeDataM = new PlaneDataM(floats[0], floats[1], floats[2], floats[3], floats[6]);
+                planeDataM = new PlaneDataM(floats[0], floats[1], floats[2], floats[3], floats[6]);
 
                 //test
-                planeDataM = GeneratorAsync.GetTestData().Result;
+                //planeDataM = GeneratorAsync.GetTestData().Result;
             }
 
             p += floatArrayLengthBytes;
